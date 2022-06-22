bacb515f0035a54a120dad34f5f065fb3ea26153
Kunal Rustagi <kunal.rustagi@gmail.com>
2022-06-22 12:03:39 +0530

    added updated merge sorted LL

--- LeetCode - Python/0002_merge_sorted_LL.py
+++ LeetCode - Python/0002_merge_sorted_LL.py
@@ -5,34 +5,66 @@ class ListNode:
 
 def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         
-    final_list = ListNode()
-    head = final_list
-    
-    while list1.next != None or list2.next != None:
+        final_list = None
+        head = None
         
-        if list1.val <= list2.val:
-            temp = ListNode(list1.val)
+        while list1 != None and list2 != None:
             
-            if final_list == head:
-                head = temp
-                final_list = head
+            if list1.val <= list2.val:
+                temp = ListNode(list1.val)
                 
+                if head == None:
+                    head = temp
+                    final_list = head
+                    
+                else:
+                    final_list.next = temp
+                    final_list = final_list.next
+                print("1:", temp.val, head.val)
+                list1 = list1.next
+                
+            
             else:
-                final_list.next = temp
-                final_list = final_list.next
-            list1 = list1.next
+                temp = ListNode(list2.val)
+                
+                if head == None:
+                    head = temp
+                    final_list = head
+                    
+                else:
+                    final_list.next = temp
+                    final_list = final_list.next
+                print("2:", temp.val, head.val)
+                list2 = list2.next
         
-        else:
+        
+        while list1 == None and list2 != None:
+            
             temp = ListNode(list2.val)
             
-            if final_list == head:
+            if head == None:
                 head = temp
                 final_list = head
-                
+
             else:
                 final_list.next = temp
                 final_list = final_list.next
+            print("3:", temp.val, head.val)
             list2 = list2.next
-    
-    return head
+        
+        while list2 == None and list1 != None:
+            
+            temp = ListNode(list1.val)
+            
+            if head == None:
+                head = temp
+                final_list = head
+
+            else:
+                final_list.next = temp
+                final_list = final_list.next
+            print("4:", temp.val, head.val)
+            list1 = list1.next
                 
+            
+        return head
